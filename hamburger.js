bead88696b71b510d0b064efba4a4168a7f8365e
2spuki <30755330+2spuki@users.noreply.github.com>
2023-04-27 11:30:47 -0400

    Delete hamburger.js

--- hamburger.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector(".nav-links");
-
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  navLinks.classList.toggle("show");
-});
