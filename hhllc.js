4ec61afe4ce59f2778adac1b578cdf0c1cdfb903
2spuki <30755330+2spuki@users.noreply.github.com>
2023-05-22 13:27:16 -0400

    Update hhllc.js

--- hhllc.js
+++ hhllc.js
@@ -1,43 +1,28 @@
-function toggle_submenu() {
-    var x = document.getElementById("sub_menu");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+            function toggle_submenu() {
+                var x = document.getElementById("sub_menu");
+                if (x.style.display === "block") {
+                    x.style.display = "none";
+                } else {
+                    x.style.display = "block";
+                }
+            }
 
-function toggle_menu() {
-    var x = document.getElementById("nav-links");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-        document.getElementById("sub_menu").style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+            function toggle_menu() {
+                var x = document.getElementById("nav-links");
+                if (x.style.display === "block") {
+                    x.style.display = "none";
+                    document.getElementById("sub_menu").style.display = "none";
+                } else {
+                    x.style.display = "block";
+                }
+            }
 
-function checkViewport() {
-    if (window.innerWidth <= 767) {
-        document.getElementById("menu").style.display = "flex";
-        document.getElementById("desktop-nav").style.display = "none";
-        document.getElementById("mobile-nav").style.display = "block";
-    } else {
-        document.getElementById("menu").style.display = "none";
-        document.getElementById("desktop-nav").style.display = "block";
-        document.getElementById("mobile-nav").style.display = "none";
-    }
-}
+            const hamMenu = document.querySelector('#menu');
+            hamMenu.addEventListener('click', () => {
+                toggle_menu();
+            });
 
-window.addEventListener('DOMContentLoaded', checkViewport);
-window.addEventListener('resize', checkViewport);
-
-const hamMenu = document.querySelector('#menu');
-hamMenu.addEventListener('click', () => {
-    toggle_menu();
-});
-
-const subMenu = document.querySelector('#sub_opener');
-subMenu.addEventListener('click', () => {
-    toggle_submenu();
-});
+            const subMenu = document.querySelector('#sub_opener');
+            subMenu.addEventListener('click', () => {
+                toggle_submenu();
+            });
