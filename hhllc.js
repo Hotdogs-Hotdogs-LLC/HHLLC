b1a53c32d7a0d635c5f4e7e0c6a619421613fbfb
2spuki <30755330+2spuki@users.noreply.github.com>
2023-05-23 11:18:39 -0400

    Update hhllc.js

--- hhllc.js
+++ hhllc.js
@@ -1,28 +1,30 @@
-function toggle_submenu() {
-    var x = document.getElementById("sub_menu");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+        function toggle_submenu() {
+                var x = document.getElementById("sub_menu");
+                if (x.style.display === "block") {
+                    x.style.display = "none";
+                } else {
+                    x.style.display = "block";
+                }
+            }
 
-function toggle_menu() {
-    var x = document.getElementById("nav-links");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-        document.getElementById("sub_menu").style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+         function toggle_menu() {
+                var x = document.getElementById("nav-links");
+                if (x.style.display === "block") {
+                    x.style.display = "none";
+                    document.getElementById("sub_menu").style.display = "none";
+                } else {
+                    x.style.display = "block";
+                }
+            }
 
-const hamMenu = document.querySelector('#menu');
-hamMenu.addEventListener('click', () => {
-    toggle_menu();
-});
+            const hamMenu = document.querySelector('#menu');
+            hamMenu.addEventListener('click', () => {
+                toggle_menu();
+            });
 
-const subMenu = document.querySelector('#sub_opener');
-subMenu.addEventListener('click', () => {
-    toggle_submenu();
-});
+            const subMenu = document.querySelector('#sub_opener');
+            subMenu.addEventListener('click', () => {
+                toggle_submenu();
+            });
+
+        
