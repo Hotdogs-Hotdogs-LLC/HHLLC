28e6abd8c36dfb12b1cc1d4a2478baa98024a0b6
2spuki <30755330+2spuki@users.noreply.github.com>
2023-05-23 10:44:43 -0400

    Update hhllc.js

--- hhllc.js
+++ hhllc.js
@@ -1,6 +1,28 @@
-    function toggleMenu() {
-        var navLinks = document.getElementById("nav-links");
-        var menuBtn = document.querySelector(".menu-btn");
-        menuBtn.classList.toggle("open");
-        navLinks.classList.toggle("show");
+    function toggle_submenu() {
+        var x = document.getElementById("sub_menu");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
     }
+
+    function toggle_menu() {
+        var x = document.getElementById("nav-links");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+            document.getElementById("sub_menu").style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
+
+    const hamMenu = document.querySelector('#menu');
+    hamMenu.addEventListener('click', () => {
+        toggle_menu();
+    });
+
+    const subMenu = document.querySelector('#sub_opener');
+    subMenu.addEventListener('click', () => {
+        toggle_submenu();
+    });
